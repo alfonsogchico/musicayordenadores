d2899d165a98a65daea82e1bbc4bdbd66a958d8d
alfonsogchico <alfonsogustavochico@gmail.com>
2024-10-07 01:04:10 +0200

    Quartz sync: Oct 7, 2024, 1:04 AM

---
+++ content/index.md
@@ -0,0 +1,9 @@
+---
+title: Home
+draft: false
+tags:
+  - example-tag
+date: 2024-10-06
+---
+ 
+The rest of your content lives here. You can use **Markdown** here :)
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,23 +8,23 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Música y ordenadores",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "es-ES",
+    baseUrl: "alfonsogchico.github.io/musicayordenadores",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Fjalla One",
+        body: "Roboto",
         code: "IBM Plex Mono",
       },
       colors: {
